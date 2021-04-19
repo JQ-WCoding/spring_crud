5b34e9fc42b77f1f11f486ecd65cc30b388da25e
SpellBound <junlee36520620@gamil.com>
2021-04-19 23:49:22 +0900

    testing

--- src/main/java/com/frappe/frappenews/NewsApplication.java
+++ src/main/java/com/frappe/frappenews/NewsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NewsApplication {
-    // for git
+    // for git testing commit
     public static void main(String[] args) {
         SpringApplication.run(NewsApplication.class, args);
     }
